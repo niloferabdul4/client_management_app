7df56511eb98a4d647ab8f6e62789de0c5d81acc
niloferabdul4 <niloferabdul4@gmail.com>
2023-03-20 14:34:08 +0400

    updated API

---
+++ src/Pages/Home/homestyles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { mobile } from "../../Responsive";
+
+const HomeContainer=styled.div`
+  width:1544px;
+  background-color: brown;
+  ${mobile({
+         display:'flex'    ,
+         flexDirection:'column'     
+         })}
+
+`
+
+
+const Mainbar=styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    position: absolute;
+    width: 1400px;
+    left: 350px;
+    gap:20px;
+    top: 89px;
+    box-shadow: rgba(53, 48, 48, 0.15) 2px 2px 2px 2px;
+    border-radius: 8px;
+    margin:30px 10px;
+    border: 0.5px solid lightgrey;
+     
+`
+export {HomeContainer,Mainbar}
